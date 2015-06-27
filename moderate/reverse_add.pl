7820876d5c9562722ab2676448381dba9c2c627a
Erendis <mailbox.novaka@gmail.com>
2015-06-28 01:12:52 +0200

    2 moderate challenges solved

---
+++ moderate/reverse_add.pl
@@ -0,0 +1,15 @@
+#!/usr/bin/perl -w
+use strict;
+
+open FH, "<", $ARGV[0] or die $!;
+
+while (<FH>)
+{
+	chomp;
+	my $n = $_;
+	my $i = 0;
+	while ($n !~ m/^((.)(?1)\2|.?)$/) { $n += reverse $n; $i++; }
+	print "$i $n\n";
+}
+
+close FH;
