e1d07c04c03d578dda1ffc12686f618044addd57
Erendis <mailbox.novaka@gmail.com>
2015-06-27 18:58:13 +0200

    bit positions

---
+++ bit_positions.pl
@@ -0,0 +1,22 @@
+#!/usr/bin/perl -w
+use strict;
+
+sub dec2bin
+{
+    my $str = unpack("B32", pack("N", shift));
+    $str =~ s/^0+(?=\d)//;
+    return $str;
+}
+
+open FH, "<", $ARGV[0] or die $!;
+
+while (<FH>)
+{
+	my ($n,$b1,$b2) = split ",", $_;
+	my @n = split "", dec2bin($n);
+	print $n[-$b1] eq $n[-$b2] ? "true\n" : "false\n";
+}
+
+close FH;
+
+
