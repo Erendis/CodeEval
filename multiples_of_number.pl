8b89fde3a5af7f03cf0478d4a359bcb400bb2bb9
Erendis <mailbox.novaka@gmail.com>
2015-06-27 17:19:59 +0200

    multiples of a number

---
+++ multiples_of_number.pl
@@ -0,0 +1,14 @@
+#!/usr/bin/perl -w
+use strict;
+
+open FH, "<", $ARGV[0] or die $!;
+
+while (<FH>)
+{
+	chomp;
+	my ($x,$n) = split ",", $_; my $i = 0;
+	while($x >= $n*++$i) { }
+	print $n*$i . "\n";
+}
+
+close FH;
