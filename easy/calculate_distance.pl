5aee58066be287652e77e8b8b539053aab2b3fbc
Erendis <mailbox.novaka@gmail.com>
2015-06-30 23:19:55 +0200

    calculate distance

---
+++ easy/calculate_distance.pl
@@ -0,0 +1,12 @@
+#!/usr/bin/perl -w
+use strict;
+
+open FH, "<", $ARGV[0] or die $!;
+
+while (<FH>)
+{
+	$_ =~ s/[\(\),]//g; 	my @n = split " ", $_;
+	print sqrt(($n[2]-$n[0])**2 + ($n[3]-$n[1])**2) . "\n";
+}
+
+close FH;
