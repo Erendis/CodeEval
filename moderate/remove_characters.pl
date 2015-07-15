60bdf724d2a1afccb11cbf557d76855983f30758
Erendis <mailbox.novaka@gmail.com>
2015-07-15 10:23:58 +0200

    s/tabs/spaces/

--- moderate/remove_characters.pl
+++ moderate/remove_characters.pl
@@ -5,9 +5,9 @@ open FH, "<", $ARGV[0] or die $!;
 
 while (<FH>)
 {
-	my ($s,$c) = split ", ", $_;
-	$s =~ s/[$c]//g;
-	print $s . "\n";
+    my ($s,$c) = split ", ", $_;
+    $s =~ s/[$c]//g;
+    print $s . "\n";
 }
 
 close FH;
--- moderate/reverse_add.pl
+++ moderate/reverse_add.pl
@@ -5,11 +5,11 @@ open FH, "<", $ARGV[0] or die $!;
 
 while (<FH>)
 {
-	chomp;
-	my $n = $_;
-	my $i = 0;
-	while ($n !~ m/^((.)(?1)\2|.?)$/) { $n += reverse $n; $i++; }
-	print "$i $n\n";
+    chomp;
+    my $n = $_;
+    my $i = 0;
+    while ($n !~ m/^((.)(?1)\2|.?)$/) { $n += reverse $n; $i++; }
+    print "$i $n\n";
 }
 
 close FH;
