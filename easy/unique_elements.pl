2df409602b28fb7d23e813998528f2e1b1c11fbf
Erendis <mailbox.novaka@gmail.com>
2015-07-15 09:41:07 +0200

    s/tabs/spaces

--- easy/unique_elements.pl
+++ easy/unique_elements.pl
@@ -5,10 +5,10 @@ open FH, "<", $ARGV[0] or die $!;
 
 while (<FH>)
 {
-	chomp;
-	my @n = split ",", $_; my $l; my $p = -1;
-	for (0..$#n) { $l .= "$n[$_]," if $n[$_] != $p; $p = $n[$_]; }
-	print substr($l, 0, -1) . "\n";
+    chomp;
+    my @n = split ",", $_; my $l; my $p = -1;
+    for (0..$#n) { $l .= "$n[$_]," if $n[$_] != $p; $p = $n[$_]; }
+    print substr($l, 0, -1) . "\n";
 }
 
 close FH;
