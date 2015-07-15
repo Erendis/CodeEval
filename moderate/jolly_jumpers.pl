0f1d136a8806e186a7403ab9acee638bc0a0a70c
Erendis <mailbox.novaka@gmail.com>
2015-07-15 10:18:36 +0200

    ...

--- moderate/jolly_jumpers.pl
+++ moderate/jolly_jumpers.pl
@@ -5,15 +5,15 @@ open FH, "<", $ARGV[0] or die $!;
 
 while (<FH>)
 {
-    chomp;
-    my @n = split " ", $_; my $n = shift @n; my @s = (0) x ($n-1);
+	chomp;
+	my @n = split " ", $_; my $n = shift @n; my @s = (0) x ($n-1);
 
-    for my $i (1..$#n)
-    {
-        my $d = abs($n[$i-1]-$n[$i]);
-        $s[$d-1]++ unless ($d > ($n-1) || $d == 0);
-    }
-    print "@s" =~ m/0/ ? "Not jolly\n" : "Jolly\n";
+	for my $i (1..$#n)
+	{
+		my $d = abs($n[$i-1]-$n[$i]);
+		$s[$d-1]++ unless ($d > ($n-1) || $d == 0);
+	}
+	print "@s" =~ m/0/ ? "Not jolly\n" : "Jolly\n";
 }
 
 close FH;
